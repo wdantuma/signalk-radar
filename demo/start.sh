429cd81018e153f748a5ab6c9017a300cd10f8f0
Willem Dantuma <willem.dantuma@dobs.nl>
2024-08-26 13:21:55 +0200

    Add mayara build

--- demo/start.sh
+++ demo/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 tcpreplay -q -T select -l 0 -i lo navico_and_0183.pcap&
 signalk-radar/radar-server --udp-source --type navico&
+mayara/mayara&
 signalk-server/bin/signalk-server -c signalk/
