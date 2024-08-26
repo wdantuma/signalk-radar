807fba1e16ea27b23ec4369a4b4aa7e0f84bfbda
Willem Dantuma <willem.dantuma@dobs.nl>
2024-08-26 14:55:30 +0200

    Update mayara use smaler pcap

--- demo/start.sh
+++ demo/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-tcpreplay -q -T select -l 0 -i lo navico_and_0183.pcap&
+tcpreplay -q -T select -l 0 -i lo halo_and_0183.pcap&
 signalk-radar/radar-server --udp-source --type navico&
-mayara/mayara&
+mayara/mayara -i lo -p 3002 --replay&
 signalk-server/bin/signalk-server -c signalk/
