046086c25c16733838b7e489976b9742d76afd8b
Willem Dantuma <willem.dantuma@dobs.nl>
2024-11-18 10:48:55 +0100

    Update signalk-radar, mayara and signalk-server

--- demo/start.sh
+++ demo/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 tcpreplay -q -T select -l 0 -i lo halo_and_0183.pcap&
 signalk-radar/radar-server --udp-source --type navico&
-mayara/mayara -i lo -p 3002 --replay&
+./mayara -i lo -p 3002 --replay&
 signalk-server/bin/signalk-server -c signalk/
